efed92735ffeeccdab93667247a7e1e0b8f7cb2b
ShingoYadomoto <shingo.yadomoto@gmail.com>
2018-08-25 02:38:42 +0900

    全体を簡素化

--- models/tasks.go
+++ models/tasks.go
@@ -1,11 +1,5 @@
 package models
 
-import (
-	"database/sql"
-
-	_ "github.com/mattn/go-sqlite3"
-)
-
 // Task is a struct containing Task data
 type Task struct {
 	ID   int    `json:"id"`
@@ -17,70 +11,14 @@ type TaskCollection struct {
 	Tasks []Task `json:"items"`
 }
 
-// GetTasks from the DB
-func GetTasks(db *sql.DB) TaskCollection {
-	sql := "SELECT * FROM tasks"
-	rows, err := db.Query(sql)
-	// Exit if the SQL doesn't work for some reason
-	if err != nil {
-		panic(err)
-	}
-	// make sure to cleanup when the program exits
-	defer rows.Close()
-
-	result := TaskCollection{}
-	for rows.Next() {
-		task := Task{}
-		err2 := rows.Scan(&task.ID, &task.Name)
-		// Exit if we get an error
-		if err2 != nil {
-			panic(err2)
-		}
-		result.Tasks = append(result.Tasks, task)
-	}
-	return result
-}
-
-// PutTask into DB
-func PutTask(db *sql.DB, name string) (int64, error) {
-	sql := "INSERT INTO tasks(name) VALUES(?)"
-
-	// Create a prepared SQL statement
-	stmt, err := db.Prepare(sql)
-	// Exit if we get an error
-	if err != nil {
-		panic(err)
-	}
-	// Make sure to cleanup after the program exits
-	defer stmt.Close()
-
-	// Replace the '?' in our prepared statement with 'name'
-	result, err2 := stmt.Exec(name)
-	// Exit if we get an error
-	if err2 != nil {
-		panic(err2)
+// GetTasks
+func GetTasks() (tc TaskCollection) {
+	tc = TaskCollection{
+		[]Task{
+			{1, "洗濯"},
+			{2, "掃除"},
+		},
 	}
 
-	return result.LastInsertId()
-}
-
-// DeleteTask from DB
-func DeleteTask(db *sql.DB, id int) (int64, error) {
-	sql := "DELETE FROM tasks WHERE id = ?"
-
-	// Create a prepared SQL statement
-	stmt, err := db.Prepare(sql)
-	// Exit if we get an error
-	if err != nil {
-		panic(err)
-	}
-
-	// Replace the '?' in our prepared statement with 'id'
-	result, err2 := stmt.Exec(id)
-	// Exit if we get an error
-	if err2 != nil {
-		panic(err2)
-	}
-
-	return result.RowsAffected()
+	return
 }
